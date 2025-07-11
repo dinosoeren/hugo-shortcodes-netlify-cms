bde5c05f404b92c55e09ab7ab50bc151f050f5af
soeren <smnbw@protonmail.com>
2025-07-10 22:33:03 -0600

    Add underscore to youtube id regex

--- src/youtube.js
+++ src/youtube.js
@@ -6,7 +6,7 @@ CMS.registerEditorComponent({
         label: "Youtube Video ID",
         widget: "string"
     }],
-    pattern: /{{< youtube\s+(?<id>[A-Za-z0-9\-]+)\s+>}}/,
+    pattern: /{{< youtube\s+(?<id>[A-Za-z0-9\-_]+)\s+>}}/,
     fromBlock: function(match) {
         return {
             id: match[1],
