3897a1d4055d5664c373b3dc405e26fa71d3b109
dinosoeren <smnbw@protonmail.com>
2025-07-13 20:17:41 -0600

    Full figure support

--- src/figure.js
+++ src/figure.js
@@ -13,10 +13,50 @@ CMS.registerEditorComponent({
         name: "src",
         label: "Image",
         widget: "image"
+    }, {
+        name: "width",
+        label: "Width",
+        widget: "number"
+    }, {
+        name: "height",
+        label: "Height",
+        widget: "number"
+    }, {
+        name: "loading",
+        label: "Loading",
+        widget: "string"
+    }, {
+        name: "class",
+        label: "Class",
+        widget: "string"
+    }, {
+        name: "link",
+        label: "Link",
+        widget: "string"
+    }, {
+        name: "target",
+        label: "Target",
+        widget: "string"
+    }, {
+        name: "rel",
+        label: "Rel",
+        widget: "string"
+    }, {
+        name: "caption",
+        label: "Caption",
+        widget: "text"
+    }, {
+        name: "attr",
+        label: "Attribution",
+        widget: "string"
+    }, {
+        name: "attrlink",
+        label: "Attribution Link",
+        widget: "string"
     }],
     pattern: /{{<\s*figure(.*)>}}/,
     fromBlock: function (input) {
-        let output = {alt: "", src: "", title: ""}
+        let output = {alt: "", src: "", title: "", width: "", height: "", loading: "", class: "", link: "", target: "", rel: "", caption: "", attr: "", attrlink: ""}
         let options = input[1].match(/\w+\s*=\s*"[^"]*"/g);
         if (options) {
             options.forEach((i) => {
@@ -31,11 +71,45 @@ CMS.registerEditorComponent({
         options += obj.src ? ` src="${obj.src}"` : '';
         options += obj.alt ? ` alt="${obj.alt}"` : '';
         options += obj.title ? ` title="${obj.title}"` : '';
-        return `{{< figure ${options}>}}`;
+        options += obj.width ? ` width="${obj.width}"` : '';
+        options += obj.height ? ` height="${obj.height}"` : '';
+        options += obj.loading ? ` loading="${obj.loading}"` : '';
+        options += obj.class ? ` class="${obj.class}"` : '';
+        options += obj.link ? ` link="${obj.link}"` : '';
+        options += obj.target ? ` target="${obj.target}"` : '';
+        options += obj.rel ? ` rel="${obj.rel}"` : '';
+        options += obj.caption ? ` caption="${obj.caption}"` : '';
+        options += obj.attr ? ` attr="${obj.attr}"` : '';
+        options += obj.attrlink ? ` attrlink="${obj.attrlink}"` : '';
+        return `{{< figure${options}>}}`;
     },
-    toPreview: ({title, alt, src}, getAsset, fields) => {
+    toPreview: (obj, getAsset, fields) => {
+        const {title, alt, src, width, height, loading, class: className, link, target, rel, caption, attr, attrlink} = obj;
         const imageField = fields?.find(f => f.get('widget') === 'image');
         const imgSrc = getAsset(src, imageField);
-        return `<figure><img src="${imgSrc}" alt="${alt}"><figcaption><h4>${title}</h4></figcaption></figure>`;
+        let imgTag = `<img src="${imgSrc}" alt="${alt || ''}"`;
+        imgTag += width ? ` width="${width}"` : '';
+        imgTag += height ? ` height="${height}"` : '';
+        imgTag += loading ? ` loading="${loading}"` : '';
+        imgTag += '>';
+        let imgWrapped = imgTag;
+        if (link) {
+            imgWrapped = `<a href="${link}"${target ? ` target="${target}"` : ''}${rel ? ` rel="${rel}"` : ''}>${imgTag}</a>`;
+        }
+        let figcaption = '';
+        if (title || caption || attr) {
+            figcaption = '<figcaption>';
+            if (title) figcaption += `<h4>${title}</h4>`;
+            if (caption) figcaption += `<div>${caption}</div>`;
+            if (attr) {
+                if (attrlink) {
+                    figcaption += ` <a href="${attrlink}">${attr}</a>`;
+                } else {
+                    figcaption += ` ${attr}`;
+                }
+            }
+            figcaption += '</figcaption>';
+        }
+        return `<figure${className ? ` class="${className}"` : ''}>${imgWrapped}${figcaption}</figure>`;
     },
 });
